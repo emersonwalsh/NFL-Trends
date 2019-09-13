64adfbab835a7f6b8530ec1c9684b6fc131397e1
Emerson Walsh <emersonwalsh@Emersons-MacBook-Pro.local>
2019-09-13 00:38:09 -0400

    ADDED
    initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+$("#cardinals").click(function() {
+    console.log('ASDFASDFAS')
+    // $('.cards').toggleClass('hidden');
+    $('#cardinals-card').toggleClass('dashboard');
+    // $('#falcons-card').toggleClass('hidden');
+
+});
+
+// $("#nfl-logo").click(function() {
+//     console.warn('Header')
+//     $('.cards').toggleClass('hidden');
+// });
